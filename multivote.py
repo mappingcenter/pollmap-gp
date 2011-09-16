79a2dc021b645a54f9189978b100d2e60255ecae
alex6294@NIFLHEIM.esri.com <alex6294@NIFLHEIM.esri.com>
2011-09-16 16:41:09 -0700

    fixed broken data source (new name convention: features) and reformatted the comments on the tracebacks

--- multivote.py
+++ multivote.py
@@ -20,7 +20,7 @@ def status(msg): #logging function
 
 #CONFIG
 db = r"\\niflheim\data\SDE_CONNECTIONS\niflheim_express1_collegefootball_webeditor.sde"
-features = db  + "\\collegefootball.DBO.zips_winkel"
+features = db  + "\\collegefootball.DBO.features"
 votes_table = db + "\\collegefootball.DBO.votes"
 
 uid_field = "COOKIE"
@@ -101,26 +101,26 @@ except arcpy.ExecuteError:
     #
     print msgs
 
-#except: # disable all this for prod
-    ## Get the traceback object
-    ##
-    #tb = exc_info()[2]
-    #tbinfo = traceback.format_tb(tb)[0]
-
-    ## Concatenate information together concerning the error into a message string
-    ##
-    #pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(exc_info()[1])
-    #msgs = "ArcPy ERRORS:\n" + arcpy.GetMessages(2) + "\n"
-
-    ## Return python error messages for use in script tool or Python Window
-    ##
-    #arcpy.AddError(pymsg)
-    #arcpy.AddError(msgs)
-
-    ## Print Python error messages for use in Python / Python Window
-    ##
-    #print pymsg + "\n"
-    #print msgs
+##except: # disable all this for prod
+##     #Get the traceback object
+##
+##    tb = exc_info()[2]
+##    tbinfo = traceback.format_tb(tb)[0]
+##
+##     #Concatenate information together concerning the error into a message string
+##
+##    pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(exc_info()[1])
+##    msgs = "ArcPy ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+##
+##    #Return python error messages for use in script tool or Python Window
+##
+##    arcpy.AddError(pymsg)
+##    arcpy.AddError(msgs)
+##
+##    # Print Python error messages for use in Python / Python Window
+##
+##    print pymsg + "\n"
+##    print msgs
 
 
 
